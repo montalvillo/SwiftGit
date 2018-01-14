dc31511faa37b2cb44acfd4ac7d3bba32acac19b
montalvillo <montalvillo@outlook.com>
2018-01-14 19:52:55 +0100

    Add files via upload

---
+++ Clase_22_Genericos/Clase_22_Genericos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clase_22_Genericos
+//
+//  Created by Juan Garcia Montalvillo on 14/1/18.
+//  Copyright © 2018 Juan Garcia Montalvillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
